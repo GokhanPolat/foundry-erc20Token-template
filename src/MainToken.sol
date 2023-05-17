39b03ecc7ed0174fa479140c60a9453adad5563e
dev | HOME <gokhan.plt@gmail.com>
2023-05-17 20:46:51 +0300

    ADDED - dotfiles

--- src/MainToken.sol
+++ src/MainToken.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
 contract MainToken {
   uint256 public number;
 
